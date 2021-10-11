6977733ec75dc4e4d27a01354003095f7ce20282
KavinduJayathilaka <70713220+KavinduJayathilaka@users.noreply.github.com>
2021-10-11 16:47:04 +0530

    Update employee record system.c
    case 3 updated

--- employee record system.c
+++ employee record system.c
@@ -106,6 +106,29 @@ int main()
 
         case '3':  /// if user press 3 then do editing existing record
            //case 3 methana
+           system("cls");
+            another = 'y';
+            while(another == 'y')
+            {
+                printf("Enter the employee name to modify: ");
+                scanf("%s", empname);
+                rewind(fp);
+                while(fread(&e,recsize,1,fp)==1)  /// fetch all record from file
+                {
+                    if(strcmp(e.name,empname) == 0)  ///if entered name matches with that in file
+                    {
+                        printf("\nEnter new name,age and bs: ");
+                        scanf("%s%d%f",e.name,&e.age,&e.bs);
+                        fseek(fp,-recsize,SEEK_CUR); /// move the cursor 1 step back from current position
+                        fwrite(&e,recsize,1,fp); /// override the record
+                        break;
+                    }
+                }
+                printf("\nModify another record(y/n)");
+                fflush(stdin);
+                another = getche();
+            }
+            
             break;
         case '4':
             system("cls");
